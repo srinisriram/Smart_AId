4d71afee441ef59f2e127ce87086e18746eb05d7
srinivassriram <Cheenu06!>
2020-09-10 20:16:56 -0400

    First commit to the Attendance System

---
+++ Product_Line_Code/Automated_Attendance_System/constants.py
@@ -0,0 +1,34 @@
+prototxt_path = "face_detection_model/deploy.prototxt"
+
+model_path = "face_detection_model/res10_300x300_ssd_iter_140000.caffemodel"
+
+embedder_path = "face_embedding_model/openface_nn4.small2.v1.t7"
+
+recognizer_path = "face_recognizer_pickle/recognizer.pickle"
+
+labels_path = "face_recognizer_pickle/labels.pickle"
+
+abhisar_sound_path = "sound_files/abhisar.wav"
+
+srinivas_sound_path = "sound_files/srinivas.wav"
+
+aditya_sound_path = "sound_files/aditya.wav"
+
+COLORS = [(255,0,0), (0,191,255), (50,205,50)]
+
+LABELS = ["Abhisar", "Srinivas", "Aditya"]
+
+frame_width_in_pixels = 320
+
+OPEN_DISPLAY = True
+
+MIN_CONFIDENCE = 0.7
+
+MIN_CONFIDENCE_FOR_FACE = 0.95
+
+MOTOR1_FORWARD_GPIO = 4
+
+ON = 1
+
+OFF = 0
+
---
+++ Product_Line_Code/Automated_Attendance_System/open_door.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as gpio
+from constants import MOTOR1_FORWARD_GPIO, ON, OFF
+import time
+
+
+class OpenDoor:
+    """
+    This class will implement the logic to open the door for both mask and attendance tracking.
+    """
+
+    def __init__(self):
+        self.setup_gpio()
+
+    @classmethod
+    def open_door(cls):
+        OpenDoor.thread_for_opening_door()
+
+    def setup_gpio(self):
+        gpio.setmode(gpio.BCM)
+        gpio.setwarnings(False)
+        gpio.setup(MOTOR1_FORWARD_GPIO, gpio.OUT)
+        gpio.output(MOTOR1_FORWARD_GPIO, OFF)
+
+    def start_motor(self):
+        """
+        This method will use the Raspberry Pi GPIO PINS to start the motor to open the door.
+        """
+        print("[INFO]: Opening Door...")
+        gpio.output(MOTOR1_FORWARD_GPIO, ON)
+
+    def stop_motor(self):
+        print("[INFO]: Stopping Motor...")
+        gpio.output(MOTOR1_FORWARD_GPIO, OFF)
+
+    def thread_for_opening_door(self):
+        self.start_motor()
+        time.sleep(3)
+        self.stop_motor()
+        time.sleep(2)
